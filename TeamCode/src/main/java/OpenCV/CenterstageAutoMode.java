9c0fc7ea632825243e1ce4b2c480e8f585ddaea5
Eduardo-Wolfbot <62482710+Eduardo-Wolfbot@users.noreply.github.com>
2023-10-31 18:25:34 -0500

    updated names to centerstage

--- TeamCode/src/main/java/OpenCV/CenterstageAutoMode.java
+++ TeamCode/src/main/java/OpenCV/CenterstageAutoMode.java
@@ -8,7 +8,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Autonomous(name="Centerstage Detecotor", group="Auto")
+@Autonomous(name="Centerstage Detector", group="Auto")
 public class CenterstageAutoMode extends LinearOpMode {
     OpenCvCamera webcam;
     @Override
